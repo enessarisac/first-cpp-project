c348a31454e8b133ea20ad263fe312da220e6111
Enes Sarısaç <sarisacenes@gmail.com>
2022-01-04 18:47:46 +0300

    Pacman finished

---
+++ Source/Pacman/EnemyPawn.cpp
@@ -0,0 +1,15 @@
+#include "EnemyPawn.h"
+
+void AEnemyPawn::Hunt()
+{
+	State = EEnemyState::Default;
+
+	StateChangedEvent.Broadcast(State);
+}
+
+void AEnemyPawn::Idle()
+{
+	State = EEnemyState::Idle;
+
+	StateChangedEvent.Broadcast(State);
+}
